1098a7838f5f55960b3c03d9ec429c5b3a33ed82
ohmuraken <kenkenmurama2@gmail.com>
2015-06-07 15:23:45 +0900

    acount2

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,6 +1,7 @@
-from flask import request, redirect, url_for, render_template, flash
+from flask import request, redirect, url_for, render_template, flash, abort, \
+        jsonify
 from flaskr import app, db
-from flaskr.models import Entry
+from flaskr.models import Entry, User
 
 @app.route('/')
 def show_entries():
@@ -18,23 +19,49 @@ def add_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
-
 @app.route('/users/')
 def user_list():
-    return 'list users'
+    users = User.query.all()
+    return render_template('user/list.html', users=users)
 
 @app.route('/users/<int:user_id>/')
 def user_detail(user_id):
-    return 'detail user ' + str(user_id)
+    user = User.query.get(user_id)
+    return render_template('user/detail.html', user=user)
 
 @app.route('/users/<int:user_id>/edit/', methods=['GET', 'POST'])
 def user_edit(user_id):
-    return 'edit user ' + str(user_id)
+    user = User.query.get(user_id)
+    if user is None:
+        abort(404)
+    if request.method == 'POST':
+        user.name=request.form['name']
+        user.email=request.form['email']
+        user.password=request.form['password']
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('user_detail', user_id=user_id))
+    return render_template('user/edit.html', user=user)
 
 @app.route('/users/create/', methods=['GET', 'POST'])
 def user_create():
-    return 'create a new user'
+    if request.method == 'POST':
+        user = User(name=request.form['name'],
+                    email=request.form['email'],
+                    password=request.form['password'])
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('user_list'))
+    return render_template('user/edit.html')
 
 @app.route('/users/<int:user_id>/delete/', methods=['DELETE'])
 def user_delete(user_id):
-    return NotImplementedError('DELETE')
+    user = User.query.get(user_id)
+    if user is None:
+        response = jsonify({'status': 'Not Found'})
+        response.status_code = 404
+        return response
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'status': 'OK'})
+
